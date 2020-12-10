f065899b7cc7506e144375845f681e6f3f9efc60
jarek ratajski <jratajski@gmail.com>
2020-12-10 17:37:17 +0100

    :exclamation:

--- src/main/kotlin/lista4/PerformanceOfMList.kt
+++ src/main/kotlin/lista4/PerformanceOfMList.kt
@@ -1,16 +1,33 @@
 package lista4
 
 fun main(){
-    val timer = System.currentTimeMillis()
     val tries = 10000000
-    var myQueue = FullQueue<Int>()
-    for (i in 0..tries){
-        myQueue = myQueue.enqueue(i)
+    run {
+        val timer = System.currentTimeMillis()
+
+        var myQueue = FullQueue<Int>()
+        for (i in 0..tries) {
+            myQueue = myQueue.enqueue(i)
+        }
+        for (i in 0..tries) {
+            myQueue = myQueue.dequeue()
+        }
+        println(System.currentTimeMillis() - timer)
     }
-    for (i in 0..tries){
-        myQueue = myQueue.dequeue()
+    System.gc()
+    Thread.sleep(1000)
+    run {
+        val timer = System.currentTimeMillis()
+
+        var myQueue = FullQueue<Int>()
+        for (i in 0..tries) {
+            myQueue = myQueue.enqueue(i)
+        }
+        for (i in 0..tries) {
+            myQueue = myQueue.dequeue()
+        }
+        println(System.currentTimeMillis() - timer)
     }
-    println(System.currentTimeMillis() - timer)
 }
 // for tries = 100000 : 38608  <-- naive append
 // for tries = 100000 : 38009  <-- naive append v.2
