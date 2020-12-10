7eeccb81c3497ab8685c95ffd0751c3d9f1e9c57
Maciej Ratajski <maciejratajski1999@gmail.com>
2020-12-10 16:04:13 +0100

    fixed warnings: charactercounter.kt

--- src/main/kotlin/misc/charactercounter.kt
+++ src/main/kotlin/misc/charactercounter.kt
@@ -1,8 +1,8 @@
 import java.io.File
 
 fun characterCounter(path: String) = File(path).inputStream().readBytes()
-    .groupBy { it }.mapValues { (k, v) -> v.size}
-    .mapKeys { (k,v) -> "'${k.toChar()}' " }
+    .groupBy { it }.mapValues { (_, v) -> v.size}
+    .mapKeys { (k,_) -> "'${k.toChar()}' " }
 
 fun main(argz: Array<String>){
     argz.forEach { filename -> println(characterCounter(filename)).also { println(filename) } }
